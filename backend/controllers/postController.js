252fdb116f214558dca00009692c8548b38ac3c1
Amadus <790494168@qq.com>
2022-04-01 19:48:11 -0700

    write API: /post/updatepost

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -41,6 +41,46 @@ exports.post_create = [
   },
 ];
 
+exports.post_update = [
+  (req, res, next) => {
+    if (!(req.body.picture_urls instanceof Array)) {
+      if (typeof req.body.picture_urls === "undefined")
+        req.body.picture_urls = [];
+      else req.body.picture_urls = new Array(req.body.picture_urls);
+    }
+    next();
+  },
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors) {
+      res.send(errors);
+      return;
+    }
+    Post.findById(req.body._id).exec((err, found_post) => {
+        if (err) return next(err);
+        if (!found_post) {
+            res.send("Post not found!");
+            return;
+        }
+        found_post.title = req.body.title;
+        found_post.price = req.body.price;
+        found_post.picture_urls = req.body.picture_urls;
+        found_post.condition = req.body.condition;
+        found_post.platform = req.body.platform;
+        found_post.postal_code = req.body.postal_code;
+        found_post.description = req.body.description;
+        found_post.status = req.body.status;
+        found_post.seller = req.body.seller;
+        found_post.save((err) => {
+            if (err) return next(err);
+            res.send("Post updated!");
+            return;
+        });
+    });
+  },
+];
+
 // exports.game_create_post = [
 //   (req, res, next) => {
 //     if (!(req.body.tag instanceof Array)) {
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const post_controller = require("../controllers/postController");
 
 router.post('/createpost', post_controller.post_create);
+router.post('/updatepost', post_controller.post_update);
 
 module.exports = router;
