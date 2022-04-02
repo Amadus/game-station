0784bd54dca442f49c5484a06286874ae40258b7
Amadus <790494168@qq.com>
2022-04-01 22:37:46 -0700

    write API /comment/getcommentsbypostid

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -6,20 +6,36 @@ const async = require("async");
 const { validationResult } = require("express-validator");
 
 exports.comment_create = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors) {
-      res.send(errors);
-      return;
-    }
+  const errors = validationResult(req);
+  if (!errors) {
+    res.send(errors);
+    return;
+  }
 
-    const comment = new Comment({
-        user: req.body.user,
-        date: Date.now(),
-        content: req.body.content,
-        post: req.body.post
-    });
-    comment.save((err) => {
-        if (err) return next(err);
-        res.send("Comment created!");
+  const comment = new Comment({
+    user: req.body.user,
+    date: Date.now(),
+    content: req.body.content,
+    post: req.body.post,
+  });
+  comment.save((err) => {
+    if (err) return next(err);
+    res.send("Comment created!");
+  });
+};
+
+exports.comment_get_by_post_id = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors) {
+    res.send(errors);
+    return;
+  }
+
+  Comment.find({ post: req.params.post_id })
+    .sort({ date: -1 })
+    .populate("user")
+    .exec((err, found_comments) => {
+      if (err) return next(err);
+      res.json(found_comments);
     });
 };
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const comment_controller = require('../controllers/commentController');
+const comment_controller = require("../controllers/commentController");
 
-router.post('/createcomment', comment_controller.comment_create);
+router.post("/createcomment", comment_controller.comment_create);
+router.get(
+  "/getcommentsbypostid/:post_id",
+  comment_controller.comment_get_by_post_id
+);
 
 module.exports = router;
