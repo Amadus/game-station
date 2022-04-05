c6df622fba3d9964cbfb7abf62b57ee21217bb04
Amadus <790494168@qq.com>
2022-04-05 13:43:54 -0700

    Protect Sell page from anonymous users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Games from "./components/Games/Games";
 import Sell from "./components/Sell/Sell";
 import Profile from "./components/Profile/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const { isLoading } = useAuth0();
@@ -21,7 +22,7 @@ function App() {
           <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/games" exact element={<Games />} />
-            <Route path="/sell" exact element={<Sell />} />
+            <Route path="/sell" exact element={<ProtectedRoute protectedComponent={Sell} />} />
             <Route path="/profile" exact element={<Profile />} />
           </Routes>
         </>
