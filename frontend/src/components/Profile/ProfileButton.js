ee13148b698962792a7b2656884d763a18d2787a
yugao <gao.yu1@northeastern.edu>
2022-04-08 22:36:48 -0700

    onClose bug fixed

--- frontend/src/components/Profile/ProfileButton.js
+++ frontend/src/components/Profile/ProfileButton.js
@@ -4,10 +4,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 import MenuItem from '@mui/material/MenuItem';
 
 
+
 export default function ProfileButton() {
     const { logout } = useAuth0();
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
+
     const handleProfileClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -16,7 +18,7 @@ export default function ProfileButton() {
     };
 
     return (
-        <MenuItem onClick={handleProfileClose} >
+        <MenuItem onClick={handleProfileClose} onClose={handleProfileClose} >
             <Link className="dashboardElements"
                 to="/profile"
             >
