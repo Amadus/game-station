907771f419e6cbbf54e453ed6abcf386b257a03a
Amadus <790494168@qq.com>
2022-04-09 21:54:36 -0700

    update API update user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const user_controller = require("../controllers/userController");
 
 router.post("/", user_controller.create_user_post);
-router.post("/updateuser", user_controller.update_user_post);
+router.put("/:id", user_controller.update_user_post);
 router.get("/getuserbyname/:user_name", user_controller.get_user_by_name);
 router.get("/getuserbyid/:id", user_controller.get_user_by_id);
 
