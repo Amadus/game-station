dbb0a41c833c1f87cc6177eb8f49264d9b7eccfe
lucaspanwang <wang.pan1@northeastern.edu>
2022-04-29 19:01:53 -0700

    Add a sold-out water print for sold out items in game details page

--- frontend/src/components/Games/PictureItem.js
+++ frontend/src/components/Games/PictureItem.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function PictureItem({ gameData, imageUrl }) {
+  const [isSold, setIsSold] = useState(false);
+
+  useEffect(() => {
+    if (gameData.status === "Sold") {
+      setIsSold(true);
+    } else {
+      setIsSold(false);
+    }
+  }, [gameData]);
+
   return (
-    <div
-      id="picture-area-details"
-      alt={gameData.title}
-      style={{
-        background: `url(${imageUrl}) center center/cover no-repeat`,
-      }}
-    >
-      <div className="blur">
-        <img
-          src={imageUrl}
-          alt={gameData.title}
-          id="hd-image"
-        />
+    <div className="picture-item">
+      <div
+        id="picture-area-details"
+        alt={gameData.title}
+        style={{
+          background: `url(${imageUrl}) center center/cover no-repeat`,
+        }}
+      >
+        <div className="blur">
+          <img src={imageUrl} alt={gameData.title} id="hd-image" />
+        </div>
       </div>
+      {isSold && (
+        <div
+          className="soldout-details"
+          style={{
+            background: `url(/images/sold-out.png) center center/cover no-repeat`,
+          }}
+        ></div>
+      )}
     </div>
   );
 }
