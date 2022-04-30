011d3417395e8cc102a6c3b929f4e3b5f7e41c25
lucaspanwang <wang.pan1@northeastern.edu>
2022-04-29 22:51:59 -0700

    Add dates for each comment

--- frontend/src/components/Games/Comment.js
+++ frontend/src/components/Games/Comment.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Avatar } from "@mui/material";
 import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 export default function Comment({ comment, me, seller }) {
   const user = comment.user;
-  const [label, setLabel] = useState("");
   const [isMe, setIsMe] = useState(false);
   const [isSeller, setIsSeller] = useState(false);
 
@@ -21,13 +21,17 @@ export default function Comment({ comment, me, seller }) {
     <div>
       <Avatar
         src={user.avatar_url}
-        sx={{ width: 35, height: 35 }}
+        sx={{ width: 40, height: 40 }}
         alt={user.user_name}
         id="sender-avatar"
       />
       <p className="sender-name">
         {user.user_name} {isMe && <span className="me-label">Me</span>}{" "}
         {isSeller && <span className="seller-label">Seller</span>}
+      </p>
+      <p className="comment-time">
+        {`${DateTime.fromISO(comment.date).toRelative()}, 
+        ${DateTime.fromISO(comment.date).toFormat("HH:mm")}`}
       </p>
       <p className="comment-text">{comment.content}</p>
     </div>
